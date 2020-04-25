2f8040542e2e8ba24af84a865da066d767df18d5
Jörn <joern.schimmelpfeng@gmail.com>
2020-04-25 12:29:46 +0200

    Enabled Swagger UI

--- src/main/java/de/dhbw/gallery/Application.java
+++ src/main/java/de/dhbw/gallery/Application.java
@@ -5,9 +5,20 @@ import io.micronaut.runtime.Micronaut;
 /**
  * Main Application starting point
  */
-@io.micronaut.core.annotation.TypeHint(typeNames = {"org.h2.Driver", "org.h2.mvstore.db.MVTableEngine"})
+@io.micronaut.core.annotation.TypeHint(typeNames = {"org.h2.Driver", "org.h2.mvstore.db.MVTableEngine"}) // needed for GraalVM
+@io.swagger.v3.oas.annotations.OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "GalleryProject",
+                version = "0.1",
+                description = "Sample API for a Photogallery student project",
+                license = @io.swagger.v3.oas.annotations.info.License(name = "Apache 2.0", url = "https://github.com/JoernSchimmelpfeng/gallery"),
+                contact = @io.swagger.v3.oas.annotations.info.Contact(
+                        url = "https://github.com/JoernSchimmelpfeng/gallery",
+                        name = "Jörn Schimmelpfeng",
+                        email = "schimmelpfeng@lehre.dhbw-stuttgart.de")
+        )
+)
 public class Application {
-
     public static void main(String[] args) {
         Micronaut.run(Application.class);
     }
